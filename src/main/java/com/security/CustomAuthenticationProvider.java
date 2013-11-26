31ad255b574e78ca67c0370d2d62d5f9b18d7e8e
chelfeb <antoxa.men@gmail.com>
2013-11-26 16:13:22 +0200

    new

--- src/main/java/com/security/CustomAuthenticationProvider.java
+++ src/main/java/com/security/CustomAuthenticationProvider.java
@@ -1,4 +1,50 @@
 package com.security;
 
-public class CustomAuthenticationProvider {
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.GrantedAuthorityImpl;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+@Component
+public class CustomAuthenticationProvider implements AuthenticationProvider {
+
+    @Override
+    public Authentication authenticate(Authentication authentication) throws AuthenticationException {
+
+        String name = authentication.getName();
+        String password = authentication.getCredentials().toString();
+
+        if (name.equals("admin1") && password.equals("admin1")) {
+
+            Collection<GrantedAuthority> grantedAuths = new ArrayList<GrantedAuthority>();
+            grantedAuths.add(new SimpleGrantedAuthority("ROLE_USER"));
+
+           // List<GrantedAuthority> authorities = new ArrayList<GrantedAuthority>();
+           // authorities.add(new GrantedAuthorityImpl("ROLE_ADMIN"));
+
+            Authentication auth = new UsernamePasswordAuthenticationToken(name, password, grantedAuths);
+
+            SecurityContextHolder.getContext().setAuthentication(auth);
+
+            return auth;
+
+        } else {
+            return null;
+        }
+    }
+
+    @Override
+    public boolean supports(Class<?> authentication) {
+        return authentication.equals(UsernamePasswordAuthenticationToken.class);
+    }
 }
